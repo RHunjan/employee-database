55cbcab82bca40132a006d7aa6a80b3e3dac6a9a
ReenaH <Reena_Hunjan@yahoo.com>
2022-07-24 15:52:12 -0400

    mvp for first two queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS role;
  
 
 
@@ -6,4 +7,19 @@ CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
     );
+
+CREATE TABLE role (
+    title VARCHAR(30) NOT NULL,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_id INTEGER,
+    salary DECIMAL NOT NULL
+);
+ 
+CREATE TABLE employee (
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    role_id INTEGER,
+    manager_id INTEGER
+);
  
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,17 @@ VALUES
     ('Finance'),
     ('Strategy'),
     ('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Manager', 80000, 1),
+    ('Sr. Manager', 100000, 2),
+    ('Head', 150000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Jane', 'Smith', 1, 1),
+    ('Ali', 'Chan', 2, 1),
+    ('Jagmeet', 'Patel', 3, 2);
+
+
