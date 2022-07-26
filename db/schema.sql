4b0db8a738f7c88e35305c6eac343d1054b2b371
ReenaH <Reena_Hunjan@yahoo.com>
2022-07-26 18:30:11 -0400

    added manager table

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,7 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
- 
 
 
 CREATE TABLE departments (
@@ -17,6 +16,11 @@ CREATE TABLE roles (
     salary DECIMAL NOT NUll,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
  );
+
+ CREATE TABLE managers (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    manager_name VARCHAR(30)
+  );
  
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -24,5 +28,6 @@ CREATE TABLE employees (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id)
  );
--- db/seeds.sql
+++ db/seeds.sql
@@ -11,9 +11,16 @@ VALUES
     ('Sr. Manager', 100000, 2),
     ('Head', 150000, 3);
 
+INSERT INTO managers(manager_name)
+VALUES
+    ('Wendy Mancho'),
+    ('Dillan Picklers'),
+    ('George Tank');
+
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
     ('Jane', 'Smith', 1, 1),
-    ('Ali', 'Chan', 2, 1),
-    ('Jagmeet', 'Patel', 3, 2);
+    ('Ali', 'Chan', 2, 2),
+    ('Jagmeet', 'Patel', 3 , NULL);
+
 
